27c4b3de71fdab2b73f8d71b9fce8e0ce679ea92
Julian <julianymark@gmail.com>
2018-05-20 09:52:25 +0200

    new and improved formula
    - that utils thing was silly
    - now ask for yesterday every 2 hours! (only really needed once per day, but in this case, even if the day should update at strange times, as long as it doesn't update more than a day in the past, then we're sure to get all the data!)

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import * as superagent from 'superagent'
 import * as cron from 'cron'
 import * as pg from 'pg'
 
-import { log, secrets, dbConnectPool, dbConnectClient } from './setup'
+import { log, secrets, dbConnectClient } from './setup'
 import * as myoauth from './oauth';
 
 const url = 'https://api.fitbit.com';
@@ -14,15 +14,6 @@ let access_token = '';
 let pgClient: pg.Client;
 
 const agent = superagent.agent();
-
-/**
- * TODO: utils.ts
- */
-function todayFormatted() {
-    const today = moment();
-    const todayFormatted = today.format('YYYY-MM-DD');
-    return todayFormatted;
-}
 
 async function getHeartData(today: string) {
     const resource = `/1/user/-/activities/heart/date/${today}/1d/1sec.json`;
@@ -63,10 +54,12 @@ async function insertHeartData(heartData: Array<{ time: string, value: number }>
     await pgClient.end()
 }
 
+/**
+ * ask for todays fresh heart readings every minute!
+ */
 new cron.CronJob('0 * * * * *', async function () {
-    pgClient = dbConnectClient();
 
-    const today = todayFormatted();
+    const today = moment().format('YYYY-MM-DD');
 
     // FIXME: maybe not generate new tokens every time? /shrug
     // FIXME: unhandled promises not caughty properly here, but Error: Unauthorized usually means get new REFRESH_TOKEN
@@ -77,6 +70,32 @@ new cron.CronJob('0 * * * * *', async function () {
         });
     log.info('NEW REFRESH_TOKEN: ' + refresh_token);
 
+    pgClient = dbConnectClient();
     const heartData = await getHeartData(today);
     await insertHeartData(heartData, today);
+
 }, null, true, null, null, true);
+
+/**
+ * ask for yesterdays data 2 hours into a new day (in case I missed some)
+ * 
+ * every 2 hours (just in case)
+ * staggered by 30 seconds becuz FIXME: data fetching herd + pgClient collision (use pool)
+ */
+new cron.CronJob('30 0 */2 * * *', async function () {
+
+    const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+
+    // FIXME: maybe not generate new tokens every time? /shrug
+    // FIXME: unhandled promises not caughty properly here, but Error: Unauthorized usually means get new REFRESH_TOKEN
+    [access_token, refresh_token] = await myoauth.refreshTokens(url, agent, refresh_token)
+        .catch((err) => {
+            log.error(err);
+            return;
+        });
+    log.info('NEW REFRESH_TOKEN: ' + refresh_token);
+
+    pgClient = dbConnectClient();
+    const heartData = await getHeartData(yesterday);
+    await insertHeartData(heartData, yesterday);
+}, null, true, null, null, false);
